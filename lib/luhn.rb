48087d8f1f7256eed714522f5cf6a8f025c49469
AMargaillan <adeline.margaillan@gmail.com>
2019-08-13 03:20:59 +0000

    error

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -2,21 +2,20 @@ module Luhn
   def self.is_valid?(number)
     num1 = number.to_s.reverse
     num2 = num1.split("").map { |x| x.to_i }
-    num2.length.times do |n|
-      if num2[n].odd?
-        num2[n] *= 2
-        if num2[n] > 9
-          num2[n] -= 9
+
+    num3 = num2.each_with_index do |item, index|
+      if index.odd?
+        item *= 2
+        if item > 9
+          item -= 9
         end
       end
     end
-    x_sum = num2.inject(0){|sum,i| sum + i }
-    if x_sum % 10 == 0
-      puts true
+
+    if num3.inject(0){ |sum,i| sum + i }  % 10 == 0
+      return true
     else
-      puts false
+      return false
     end
   end
 end
-
-Luhn.is_valid?(4194560385008504)
